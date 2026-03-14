3d4450affd092b3aee6c635de94fed9795a36147
ttibsi <ashisbitt@icloud.com>
2026-03-14 12:11:13 +0000

    w2.6 parse csv

---
+++ COM00142M_Advanced_Programming/exercises/w2.6_file_processing.py
@@ -0,0 +1,59 @@
+import datetime
+from enum import Enum
+from typing import NamedTuple
+
+class Title(Enum):
+    MR = 0,
+    MS = 1,
+    MRS = 2, 
+    DR = 3,
+
+
+class Person(NamedTuple):
+    title: Title | None
+    surname: str
+    given_names: str
+    id_: str
+    email: str
+    company: str
+    updated: datetime.date
+
+
+def get_title_enum(titleStr: str) -> Title | None:
+    if not titleStr:
+        return None
+    if "mrs" in titleStr.lower():
+        return Title.MRS
+    elif "dr" in titleStr.lower():
+        return Title.DR
+    elif "mr" in titleStr.lower():
+        return Title.MR
+    else: 
+        return Title.MS
+
+def date_to_obj(dateStr: str) -> datetime.date:
+    return datetime.datetime.strptime(dateStr, "%d/%m/%Y").date()
+
+def main() -> int:
+    with open("PeopleTrainingDate.csv", "r") as f:
+        lines = f.readlines()
+
+    lines = lines[1:-1] # strip out headers and final newline
+    data: list[Person] = []
+    for line in lines:
+        elems = line.strip().split(",")
+        given_title = get_title_enum(elems[0])
+
+        data.append(Person(
+            given_title,
+            elems[1][1:],  # strip the opening quote
+            elems[2][1:-1], # strip the closing quote and opening space
+            *elems[3:-1],
+            date_to_obj(elems[-1])
+        ))
+
+    print(data[0])
+    return 0
+
+if __name__ == "__main__":
+    raise SystemExit(main())
