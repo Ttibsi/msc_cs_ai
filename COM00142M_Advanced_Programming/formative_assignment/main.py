50f12eb15ec020698c69b86ea2e1bd921aac38c6
ttibsi <ashisbitt@icloud.com>
2026-03-24 22:17:47 +0000

    get all database building

--- COM00142M_Advanced_Programming/formative_assignment/main.py
+++ COM00142M_Advanced_Programming/formative_assignment/main.py
@@ -55,82 +55,83 @@ def setup_db() -> None:
     cur = conn.cursor()
 
     cur.execute("""
-        CREATE TABLE antenna(
+        CREATE TABLE IF NOT EXISTS antenna(
             id INTEGER NOT NULL UNIQUE,
-            NGR	TEXT,
+            NGR    TEXT,
             LongitudeLatitude TEXT,
             Site_Height INTEGER DEFAULT 0,
-            In-Use_Ae_Ht INTEGER DEFAULT 0,
-            In-Use_ERP_Total INTEGER DEFAULT 0,
-            Dir_Max_ERP	INTEGER DEFAULT 0,
-            0	REAL DEFAULT 0.0, 10 REAL DEFAULT 0.0, 20 REAL DEFAULT 0.0,
-            30	REAL DEFAULT 0.0, 4 REAL DEFAULT 0.0, 50 REAL DEFAULT 0.0,
-            60	REAL DEFAULT 0.0, 70 REAL DEFAULT 0.0, 80 REAL DEFAULT 0.0,
-            90	REAL DEFAULT 0.0, 100 REAL DEFAULT 0.0, 110	REAL DEFAULT 0.0,
-            120	REAL DEFAULT 0.0, 130 REAL DEFAULT 0.0, 140	REAL DEFAULT 0.0,
-            150	REAL DEFAULT 0.0, 160 REAL DEFAULT 0.0, 170	REAL DEFAULT 0.0,
-            180	REAL DEFAULT 0.0, 190 REAL DEFAULT 0.0, 200	REAL DEFAULT 0.0,
-            210	REAL DEFAULT 0.0, 220 REAL DEFAULT 0.0, 230	REAL DEFAULT 0.0,
-            240	REAL DEFAULT 0.0, 250 REAL DEFAULT 0.0, 260	REAL DEFAULT 0.0,
-            270	REAL DEFAULT 0.0, 280 REAL DEFAULT 0.0, 290	REAL DEFAULT 0.0,
-            300	REAL DEFAULT 0.0, 310 REAL DEFAULT 0.0, 320	REAL DEFAULT 0.0,
-            330	REAL DEFAULT 0.0, 340 REAL DEFAULT 0.0, 350	REAL DEFAULT 0.0,
-            Lat	REAL DEFAULT 0.0, Long REAL DEFAULT 0.0,
+            In_Use_Ae_Ht INTEGER DEFAULT 0,
+            In_Use_ERP_Total INTEGER DEFAULT 0,
+            Dir_Max_ERP    INTEGER DEFAULT 0,
+            '0'    REAL DEFAULT 0.0, '10' REAL DEFAULT 0.0, '20' REAL DEFAULT 0.0,
+            '30'    REAL DEFAULT 0.0, '4' REAL DEFAULT 0.0, '50' REAL DEFAULT 0.0,
+            '60'    REAL DEFAULT 0.0, '70' REAL DEFAULT 0.0, '80' REAL DEFAULT 0.0,
+            '90'    REAL DEFAULT 0.0, '100' REAL DEFAULT 0.0, '110'    REAL DEFAULT 0.0,
+            '120'    REAL DEFAULT 0.0, '130' REAL DEFAULT 0.0, '140'    REAL DEFAULT 0.0,
+            '150'    REAL DEFAULT 0.0, '160' REAL DEFAULT 0.0, '170'    REAL DEFAULT 0.0,
+            '180'    REAL DEFAULT 0.0, '190' REAL DEFAULT 0.0, '200'    REAL DEFAULT 0.0,
+            '210'    REAL DEFAULT 0.0, '220' REAL DEFAULT 0.0, '230'    REAL DEFAULT 0.0,
+            '240'    REAL DEFAULT 0.0, '250' REAL DEFAULT 0.0, '260'    REAL DEFAULT 0.0,
+            '270'    REAL DEFAULT 0.0, '280' REAL DEFAULT 0.0, '290'    REAL DEFAULT 0.0,
+            '300'    REAL DEFAULT 0.0, '310' REAL DEFAULT 0.0, '320'    REAL DEFAULT 0.0,
+            '330'    REAL DEFAULT 0.0, '340' REAL DEFAULT 0.0, '350'    REAL DEFAULT 0.0,
+            Lat    REAL DEFAULT 0.0, Long REAL DEFAULT 0.0
         );
     """)
 
     cur.execute("""
-        CREATE TABLE params(
+        CREATE TABLE IF NOT EXISTS params(
             id INTEGER NOT NULL UNIQUE,
             Date TEXT,
             Ensemble TEXT,
             Licence TEXT,
             Ensemble_Area TEXT,
-            EID	TEXT,
+            EID    TEXT,
             Transmitter_Area TEXT,
             Site TEXT,
-            Freq. REAL DEFAULT 0.0, TEXT,
+            Freq REAL DEFAULT 0.0,
             Block TEXT,
-            TII_Main_Id_(Hex) TEXT,
-            TII_Sub_Id_(Hex) TEXT,
-            Serv_Label1  TEXT, SId_1 (Hex) TEXT, LSN_1 (Hex) TEXT, Serv_Label2  TEXT,
-            SId_2 (Hex) TEXT, LSN_2 (Hex) TEXT, Serv_Label3  TEXT, SId_3 (Hex) TEXT,
-            LSN_3 (Hex) TEXT, Serv_Label4  TEXT, SId_4 (Hex) TEXT, LSN_4 (Hex) TEXT,
-            Serv_Label5 TEXT, SId_5 (Hex) TEXT, LSN_5 (Hex) TEXT, Serv_Label6  TEXT,
-            SId_6 (Hex) TEXT, LSN_6 (Hex) TEXT, Serv_Label7  TEXT, SId_7 (Hex) TEXT,
-            LSN_7 (Hex) TEXT, Serv_Label8  TEXT, SId_8 (Hex) TEXT, LSN_8 (Hex) TEXT,
-            Serv_Label9  TEXT, SId_9 (Hex) TEXT, LSN_9 (Hex) TEXT, Serv_Label10  TEXT,
-            SId_10_(Hex) TEXT, LSN_10_(Hex) TEXT, Serv_Label11  TEXT, SId_11_(Hex) TEXT,
-            LSN_11_(Hex) TEXT, Serv_Label12  TEXT, SId_12_(Hex) TEXT, LSN_12_(Hex) TEXT,
-            Serv_Label13  TEXT, SId_13_(Hex) TEXT, LSN_13_(Hex) TEXT, Serv_Label14 TEXT,
-            SId_14_(Hex) TEXT, LSN_14_(Hex) TEXT, Serv_Label15 TEXT, SId_15_(Hex) TEXT,
-            LSN_15_(Hex) TEXT, Serv_Label16  TEXT, SId_16_(Hex) TEXT, LSN_16_(Hex) TEXT,
-            Serv_Label17  TEXT, SId_17_(Hex) TEXT, LSN_17_(Hex) TEXT, Serv_Label18  TEXT,
-            SId_18_(Hex) TEXT, LSN_18_(Hex) TEXT, Serv_Label19  TEXT, SId_19_(Hex) TEXT,
-            LSN_19_(Hex) TEXT, Serv_Label20  TEXT, SId_20_(Hex) TEXT, LSN_20_(Hex) TEXT,
-            Serv_Label21  TEXT, SId_21_(Hex) TEXT, LSN_21_(Hex) TEXT, Serv_Label22  TEXT,
-            SId_22_(Hex) TEXT, LSN_22_(Hex) TEXT, Serv_Label23  TEXT, SId_23_(Hex) TEXT,
-            LSN_23_(Hex) TEXT, Serv_Label24  TEXT, SId_24_(Hex) TEXT, LSN_24_(Hex) TEXT,
-            Serv_Label25  TEXT, SId_25_(Hex) TEXT, LSN_25_(Hex) TEXT, Serv_Label26  TEXT,
-            SId_26_(Hex) TEXT, LSN_26_(Hex) TEXT, Serv_Label27  TEXT, SId_27_(Hex) TEXT,
-            LSN_27_(Hex) TEXT, Serv_Label28  TEXT, SId_28_(Hex) TEXT, LSN_28_(Hex) TEXT,
-            Serv_Label29  TEXT, SId_29_(Hex) TEXT, LSN_29_(Hex) TEXT, Serv_Label30  TEXT,
-            SId_30_(Hex) TEXT, LSN_30_(Hex) TEXT, Serv_Label31  TEXT, SId_31_(Hex) TEXT,
-            LSN_31_(Hex) TEXT, Serv_Label32  TEXT, SId_32_(Hex) TEXT, LSN_32_(Hex) TEXT,
-            Data_Serv_Label1 TEXT, Data_SId_1 (Hex) TEXT, Data_Serv_Label2 TEXT,
-            Data_SId_2 (Hex) TEXT, Data_Serv_Label3 TEXT, Data_SId_3 (Hex) TEXT,
-            Data_Serv_Label4 TEXT, Data_SId_4 (Hex) TEXT, Data_Serv_Label5 TEXT,
-            Data_SId_5 (Hex) TEXT, Data_Serv_Label6 TEXT, Data_SId_6 (Hex) TEXT,
-            Data_Serv_Label7 TEXT, Data_SId_7 (Hex) TEXT, Data_Serv_Label8 TEXT,
-            Data_SId_8 (Hex) TEXT, Data_Serv_Label9 TEXT, Data_SId_9 (Hex) TEXT,
-            Data_Serv_Label10 TEXT, Data_SId_10_(Hex) TEXT, Data_Serv_Label11 TEXT,
-            Data_SId_11_(Hex) TEXT, Data_Serv_Label12 TEXT, Data_SId_12_(Hex) TEXT,
-            Data_Serv_Label13 TEXT, Data_SId_13_(Hex) TEXT, Data_Serv_Label14 TEXT,
-            Data_SId_14_(Hex) TEXT, Data_Serv_Label15 TEXT, Data_SId_15_(Hex) TEXT,
+            TII_Main_Id TEXT,
+            TII_Sub_Id TEXT,
+            Serv_Label1  TEXT, SId_1 TEXT, LSN_1 TEXT, Serv_Label2  TEXT,
+            SId_2 TEXT, LSN_2 TEXT, Serv_Label3  TEXT, SId_3 TEXT,
+            LSN_3 TEXT, Serv_Label4  TEXT, SId_4 TEXT, LSN_4 TEXT,
+            Serv_Label5 TEXT, SId_5 TEXT, LSN_5 TEXT, Serv_Label6  TEXT,
+            SId_6 TEXT, LSN_6 TEXT, Serv_Label7  TEXT, SId_7 TEXT,
+            LSN_7 TEXT, Serv_Label8  TEXT, SId_8 TEXT, LSN_8 TEXT,
+            Serv_Label9  TEXT, SId_9 TEXT, LSN_9 TEXT, Serv_Label10  TEXT,
+            SId_10 TEXT, LSN_10 TEXT, Serv_Label11  TEXT, SId_11 TEXT,
+            LSN_11 TEXT, Serv_Label12  TEXT, SId_12 TEXT, LSN_12 TEXT,
+            Serv_Label13  TEXT, SId_13 TEXT, LSN_13 TEXT, Serv_Label14 TEXT,
+            SId_14 TEXT, LSN_14 TEXT, Serv_Label15 TEXT, SId_15 TEXT,
+            LSN_15 TEXT, Serv_Label16  TEXT, SId_16 TEXT, LSN_16 TEXT,
+            Serv_Label17  TEXT, SId_17 TEXT, LSN_17 TEXT, Serv_Label18  TEXT,
+            SId_18 TEXT, LSN_18 TEXT, Serv_Label19  TEXT, SId_19 TEXT,
+            LSN_19 TEXT, Serv_Label20  TEXT, SId_20 TEXT, LSN_20 TEXT,
+            Serv_Label21  TEXT, SId_21 TEXT, LSN_21 TEXT, Serv_Label22  TEXT,
+            SId_22 TEXT, LSN_22 TEXT, Serv_Label23  TEXT, SId_23 TEXT,
+            LSN_23 TEXT, Serv_Label24  TEXT, SId_24 TEXT, LSN_24 TEXT,
+            Serv_Label25  TEXT, SId_25 TEXT, LSN_25 TEXT, Serv_Label26  TEXT,
+            SId_26 TEXT, LSN_26 TEXT, Serv_Label27  TEXT, SId_27 TEXT,
+            LSN_27 TEXT, Serv_Label28  TEXT, SId_28 TEXT, LSN_28 TEXT,
+            Serv_Label29  TEXT, SId_29 TEXT, LSN_29 TEXT, Serv_Label30  TEXT,
+            SId_30 TEXT, LSN_30 TEXT, Serv_Label31  TEXT, SId_31 TEXT,
+            LSN_31 TEXT, Serv_Label32  TEXT, SId_32 TEXT, LSN_32 TEXT,
+            Data_Serv_Label1 TEXT, Data_SId_1 TEXT, Data_Serv_Label2 TEXT,
+            Data_SId_2 TEXT, Data_Serv_Label3 TEXT, Data_SId_3 TEXT,
+            Data_Serv_Label4 TEXT, Data_SId_4 TEXT, Data_Serv_Label5 TEXT,
+            Data_SId_5 TEXT, Data_Serv_Label6 TEXT, Data_SId_6 TEXT,
+            Data_Serv_Label7 TEXT, Data_SId_7 TEXT, Data_Serv_Label8 TEXT,
+            Data_SId_8 TEXT, Data_Serv_Label9 TEXT, Data_SId_9 TEXT,
+            Data_Serv_Label10 TEXT, Data_SId_10 TEXT, Data_Serv_Label11 TEXT,
+            Data_SId_11 TEXT, Data_Serv_Label12 TEXT, Data_SId_12 TEXT,
+            Data_Serv_Label13 TEXT, Data_SId_13 TEXT, Data_Serv_Label14 TEXT,
+            Data_SId_14 TEXT, Data_Serv_Label15 TEXT, Data_SId_15 TEXT
         );
     """)
 
-    cur.close()
+    conn.commit()
+    conn.close()
 
 
 def get_params_table() -> list[list[str]]:
@@ -143,8 +144,9 @@ def get_params_table() -> list[list[str]]:
 def get_antennas_table() -> list[list[str]]:
     conn = sqlite3.connect("db.db")
     cur = conn.cursor()
-    res = cur.execute("SELECT * FROM antenna;")
-    return res.fetchall()
+    # res = cur.execute("SELECT * FROM antenna;")
+    # return res.fetchall()
+    return []
 
 
 def parse_file(filename: str) -> None:
@@ -209,7 +211,7 @@ def build_table(
     return table
 
 
-def _submit_changes(): 
+def _submit_changes():
     ...
 
 def main() -> int:
@@ -223,7 +225,7 @@ def main() -> int:
         nonlocal win
         popup = tkinter.Toplevel(win)
         popup.title("Modify row")
-        
+
         for idx, row in enumerate(selected_frame):
             tkinter.Label(popup, row).grid(row=idx, col=0)
             tkinter.Text(popup).grid(row=idx, col=1)
