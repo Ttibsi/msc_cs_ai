fe3700d8658633bd60e3f339441a5a0812b7b082
Ttibsi <ashisbitt@icloud.com>
2026-04-14 06:56:03 +0000

    data wrangling task abandoned

--- COM00142M_Advanced_Programming/exercises/w6.4_data_wrangling.py
+++ COM00142M_Advanced_Programming/exercises/w6.4_data_wrangling.py
@@ -3,19 +3,11 @@ import pandas
 def exercise_one():
     with open("collegeGrades1.csv") as f:
         # strip lines that are only full of commas (empty lines)
-        lines = [l for l in f.readlines() if len(set(l)) > 1]
-        headings = lines[0:2]
-        body = [l.strip().split(",") for l in lines[2:]]
-        indexes = [l[0] for l in body if l[0]]
+        lines = [l.strip() for l in f.readlines() if len(set(l)) > 2]
 
-    data = pandas.DataFrame(body)
-    print(data)
-
-
-    # data = pandas.read_csv("collegeGrades1.csv")
-    # data.dropna(how="all", inplace=True)
-    # data = data.set_index([0, 1])
-    # print(data)
+    # table 1
+    header = [x for x in lines[1].split(",") if x]
+    print(header)
 
 if __name__ == "__main__":
     raise SystemExit(exercise_one())
